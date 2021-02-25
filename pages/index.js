395887c8c3fdc8478a139b2b26dd7543440c3b53
VitorFelipePatricio <vitor.felipeee88@gmail.com>
2021-02-25 17:23:11 -0300

    First commit

---
+++ pages/index.js
@@ -0,0 +1,56 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+        <div className={styles.containerCentral}>      
+            <div className={styles.containerLeft}>
+                <div>
+                    <h1 className={styles.h1}>Hello World.</h1>
+                </div>
+                <div>
+                    <p className={styles.pLeft}>So never mind the darkness We still can find a way 'Cause nothin' lasts forever Even cold November rain.</p>
+                </div>
+                <div>
+                    <h3 className={styles.h3}>Login with social media</h3>
+                    <div>
+                        <div className={styles.containerIcons}>
+                            <div className={styles.iconFacebook}>
+                                <i className="fab fa-facebook-f"></i>
+                            </div>
+                            <div className={styles.iconTwitter}>
+                                <p className={styles.p}><i className="fab fa-twitter"></i> Login with twitter</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.containerRight}>
+                <form className={styles.form} action="">
+                <h1>Login</h1>
+                    <p className={styles.pRight}>Don't have an account? <Link href='/singup'><a className={styles.a}>Create your account</a></Link> it takes less then a minute.</p>
+                    <div className={styles.containerInput}> 
+                        <input className={styles.input} type="text" placeholder="Email" name="" id="" required />
+                    </div>
+                    <div className={styles.containerInput}>
+                        <input className={styles.input} type="password" placeholder="Password" name="" id="" required />
+                    </div>
+                    <div className={styles.containerCheck}>
+                        <div>
+                            <input className={styles.inputCheck} type="checkbox" name="rememberMe" id="rememberMe" />
+                            <label className={styles.labelRemember} htmlFor="rememberMe">Remember me</label>
+                        </div>
+                        <div className={styles.forgotPassword}>
+                            <Link href='/'>
+                                <a className={styles.a}>Forgot password?</a>
+                            </Link>
+                        </div>
+                    </div>
+                    <button className={styles.btn} type="submit">Login</button>
+                </form>
+            </div>
+        </div>  
+    </div>
+  )
+}
---
+++ pages/singup.js
@@ -0,0 +1,48 @@
+import {render} from 'react-dom'
+import styles from '../styles/Singup.module.css'
+
+export default function Singup() {
+    function Submit(e) {
+        e.preventDefault()
+        const form = e.target
+        if(form.password.value === form.confirmPassword.value){
+            console.log('deu bom')
+        } else {
+            const erro = <p>Password fields must be equals</p>
+            render(erro, document.getElementById('erro'))
+        }
+
+    }
+    return(
+        <div>
+            <div className={styles.container}></div>
+            <div className={styles.containerCentral}>
+                <div className={styles.containerLeft}>
+                    <h1 className={styles.h1}>the beginning of the journey</h1>
+                    <h3 className={styles.h3}>Create an account to join the community</h3>
+                </div>
+                <div className={styles.containerRight}>
+                    <div>
+                        <form className={styles.form} onSubmit={Submit}>
+                            <h2 className={styles.h2}>Create Account</h2>
+                            <div className={styles.containerInput}>
+                                <input className={styles.input} type='email' placeholder='Email' required />
+                            </div>
+                            <div className={styles.containerInput}>
+                                <input className={styles.input} type='text' placeholder='Name' required />
+                            </div>
+                            <div className={styles.containerInput}>
+                                <input className={styles.input} type='password' placeholder='Password' name='password' required />
+                            </div>
+                            <div className={styles.containerInput}>
+                                <input className={styles.input} type='password' placeholder='Confirm password' name='confirmPassword' required />
+                            </div>
+                            <div className={styles.erro} id='erro'></div>
+                            <button className={styles.btn} type='submit'>Sing up</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>    
+    )
+}
